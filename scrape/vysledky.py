37253a29748baf5cf2f5c1ab30060be4d769002d
Pavel Potocek <pavelpotocek@gmail.com>
2019-02-22 08:13:48 +0100

    Finish the result scraping code

--- scrape/vysledky.py
+++ scrape/vysledky.py
@@ -40,6 +40,8 @@ def get_results(webpage, cat_id):
 
 
 comps = json.load(open('data/souteze.json', 'r'))
+results = json.load(open('data/vysledky.json', 'r'))
+len(results)
 
 results = {}
 for i, comp in enumerate(comps):
@@ -54,7 +56,7 @@ for i, comp in enumerate(comps):
 
         for _ in range(10):
             try:
-                r = requests.get(url, timeout=0.5)
+                r = requests.get(url, timeout=1.0)
             except (requests.ConnectTimeout, requests.ReadTimeout):
                 print(f'server couldn\'t be reached.')
             else:
@@ -68,26 +70,4 @@ for i, comp in enumerate(comps):
 
         results[cat['id']] = res
 
-
-
-comps = {}
-
-for y in range(2001, 2019):
-    for m in range(1, 13):
-        if (y, m) in comps.keys(): continue
-        print(f'getting comps for {y}-{m}..')
-        url = f'http://www.csts.cz/cs/VysledkySoutezi/Souteze?rok={y}&mesic={m:02}'
-
-
-        data = r.text
-
-        cs = get_comps(url)
-        print(f'got {len(cs)} records.')
-        comps[(y, m)] = cs
-
-
-comps_cat = []
-for comp in comps.values():
-    comps_cat += comp
-
-json.dump(comps_cat, open('data/souteze.json', 'w'), sort_keys=True, indent=4, separators=(',', ': '))
+json.dump(results, open('data/vysledky.json', 'w'), sort_keys=True, indent=4, separators=(',', ': '))
